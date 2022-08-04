6c5c6a0159da7a29e12954b6f1166802392fd109
Erin <ejagneaux@gmail.com>
2022-08-04 14:38:10 -0600

    6 imagess

--- first_game/src/Memorydeck.js
+++ first_game/src/Memorydeck.js
@@ -1,22 +1,33 @@
 import React, { Component } from "react";
 import Memorycard from "./Memorycard";
+import "./Memorydeck.css";
 
 class Memorydeck extends Component {
   static defaultProps = {
     animals: [
       { name: "Elephant" },
+      { name: "Elephant" },
+      { name: "Bee" },
       { name: "Bee" },
       { name: "Esme" },
+      { name: "Esme" },
       { name: "Octopus" },
+      { name: "Octopus" },
+      { name: "Sea Lion" },
+      { name: "Sea Lion" },
+      { name: "Wasp" },
+      { name: "Wasp" },
     ],
   };
   render() {
     return (
-      <>
-        {this.props.animals.map((a) => (
-          <Memorycard name={a.name} />
-        ))}
-      </>
+      <div className="Memorydeck">
+        <div className="Memorydeck-cards">
+          {this.props.animals.map((a) => (
+            <Memorycard name={a.name} />
+          ))}
+        </div>
+      </div>
     );
   }
 }
